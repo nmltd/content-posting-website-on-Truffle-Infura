192dc520ffcd12d060041ce22197c6a5973e85dd
Siddharth Khan <siddharthkhan951@gmail.com>
2022-05-25 18:20:58 +0530

    update 2.1

--- contracts/polygon/MyNFT.sol
+++ contracts/polygon/MyNFT.sol
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract MyNFT is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -19,10 +20,13 @@ contract MyNFT is ERC721URIStorage, Ownable {
         returns (uint256)
     {
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
         return newItemId;
+    }
+    function transfer (IERC721 _nft) public {
+        uint256 newItemId = _tokenIds.current();
+        _nft.transferFrom(msg.sender, address(this), newItemId);
     }
 }
--- migrations/mint_NFT.js
+++ migrations/mint_NFT.js
@@ -1,55 +1,59 @@
 require("dotenv").config();
+const Web3 = require("web3")
 const API_URL = process.env.API_URL;
-const { ethers } = require("ethers")
+const web3 = new Web3(new Web3.providers.HttpProvider(API_URL))
+//const { ethers } = require("ethers")
 const fs = require('fs')
-//const PUBLIC_KEY = process.env.PUBLIC_KEY;
+const PUBLIC_KEY = process.env.PUBLIC_KEY;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const provider = new ethers.providers.JsonRpcProvider(API_URL)
+//const provider = new ethers.providers.JsonRpcProvider(API_URL)
 
-const contract = require("../client/contract/polygon-contracts/MyNFT.json")
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json")
 
-console.log(JSON.stringify(contract.abi));
+//console.log(JSON.stringify(contract.abi));
+//this is the contract address we get after deploying
+const contractAddress = "0x6909e1Ee72287E883fc0A403670cC560a86B28Eb";
 
-// const contractAddress = "0x6909e1Ee72287E883fc0A403670cC560a86B28Eb";
-// const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
-// //create transaction
-// async function mintNFT(tokenURI) {
-//   const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress);
+//create transaction
+async function mintNFTandTransfer(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, "latest"); //get latest nonce
 
-//   //the transaction
-//   const tx = {
-//     from: PUBLIC_KEY,
-//     to: contractAddress,
-//     nonce: nonce,
-//     gas: 500000,
-//     data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
-//   };
+  //the transaction
+  const tx = {
+    from: PUBLIC_KEY,
+    to: contractAddress,
+    nonce: nonce,
+    gas: 500000,
+    data: nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI(),
+  };
 
-//   const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
-//   signPromise
-//     .then((signedTx) => {
-//       web3.eth.sendSignedTransaction(
-//         signedTx.rawTransaction,
-//         function (err, hash) {
-//           if (!err) {
-//             console.log(
-//               "The hash of your transaction is: ",
-//               hash,
-//               "\nCheck Alchemy's Mempool to view the status of your transaction!"
-//             );
-//           } else {
-//             console.log(
-//               "Something went wrong when submitting your transaction:",
-//               err
-//             );
-//           }
-//         }
-//       );
-//     })
-//     .catch((err) => {
-//       console.log(" Promise failed:", err);
-//     });
-// }
-// mintNFT(
-//   "https://gateway.pinata.cloud/ipfs/QmR9cmeToyY7paebR5F2CYyoPKq7CjJN33onExP2KtJTvU"
-// );
+  const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  signPromise
+    .then((signedTx) => {
+      web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction,
+        function (err, hash) {
+          if (!err) {
+            console.log(
+              "The hash of your transaction is: ",
+              hash,
+              "\nCheck https://mumbai.polygonscan.com/ to view the status of your transaction!"
+            );
+          } else {
+            console.log(
+              "Something went wrong in your transaction:",
+              err
+            );
+          }
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(" Promise failed:", err);
+    });
+    nftContract.transfer(MyNFT.address);
+}
+mintNFTandTransfer(
+  "https://gateway.pinata.cloud/ipfs/QmR9cmeToyY7paebR5F2CYyoPKq7CjJN33onExP2KtJTvU"
+);
